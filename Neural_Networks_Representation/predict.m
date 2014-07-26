a9fd2a26b7de03ee682f2e76efed2a6e4ceac220
Mike Marshall <User1@Michaels-MacBook-Pro-3.local>
2014-07-25 23:22:06 -0400

    finished prediciton

--- Neural_Networks_Representation/predict.m
+++ Neural_Networks_Representation/predict.m
@@ -21,6 +21,13 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+A1 = [ones(m,1) X];
+Z2 = sigmoid(A1*Theta1');
+A2 = [ones(m,1) Z2];
+Z3 = sigmoid(A2 * Theta2');
+A3 = sigmoid(Z3);
+[~, p] = max(A3, [], 2);
+
 
 
 
